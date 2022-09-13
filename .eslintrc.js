f7a2f9fd501cee898ce28910af6a280c1b28ce7c
Denis Stiglic <denis.stiglic@innovatrics.com>
2022-09-13 13:45:15 +0000

    DOT web integration sample release 1.0.9

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,50 @@
 module.exports = {
-  rules: {
-    "no-console": "off",
-    "@typescript-eslint/naming-convention": ["error",
-      {
-        "selector": "property",
-        "filter": "__typename",
-        "format": null
+  parser: '@typescript-eslint/parser',
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [".js", ".jsx", ".ts", ".tsx"]
       }
-    ],
-    'graphql/template-strings': [
+    }
+  },
+  parserOptions: {
+    project: ['./packages/server/tsconfig.json', './packages/client/tsconfig.json', './cypress/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  env: {
+    browser: true,
+    node: true,
+  },
+  globals: {
+    __PACKAGE_JSON_VERSION__: true,
+  },
+  root: true,
+  // 'prettier' here disables formatting-only-rules that come from the innovatrics-side,
+  // that might conflict with the prettier-settings
+  extends: [
+    '@innovatrics/eslint-config-typescript-react',
+    'prettier',
+  ],
+  plugins: ['graphql', 'react-hooks'],
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+
+    'import/extensions': [
       'error',
+      'always',
       {
-        env: 'apollo',
+        js: 'never',
+        ts: 'never',
+        tsx: 'never',
       },
     ],
-    "import/no-extraneous-dependencies": ["error", { "devDependencies": true }]
+
+    // sometimes the variable has the underscore at the beginning,
+    // for example `__typename` in graphql.
+    // also, prefixing a method-name with `_` is a simple way to
+    // signalize that a method is private-ish, without having
+    // to enforce it.
+    'no-underscore-dangle': 'off',
+    "@typescript-eslint/no-non-null-assertion": "off"
   },
-  ignorePatterns: ["api.ts", "graphqlTypes.ts"],
-}
+};
---
+++ README.md
@@ -0,0 +1,70 @@
+# DOT Web Demo
+
+## Project structure
+
+- [Server](#server) - `/packages/server`
+
+## Project setup
+
+### Prerequisites
+
+- node lts/gallium
+- yarn
+- git bash (windows users only)
+
+### Run project locally
+
+- Install yarn:
+
+```
+npm install -g yarn
+```
+
+- If you use _nvm_, select right version of _NodeJS_ with:
+
+```
+nvm use
+```
+
+- Create `.env` file in the project root directory. Copy content of `.env.sample` into `.env`.
+
+- **(Windows users only)** run `yarn win-setup` in order to setup bash shell for yarn
+
+### Available scripts
+
+Run all scripts from the project root.
+
+To install dependencies run:
+
+```
+yarn
+```
+
+To generate GraphQL types from schema:
+
+```
+yarn codegen
+```
+
+> **_Note:_** Types will be generated to _/packages/server/src/graphqlTypes.ts_ from schema located in _packages/server/src/graphQL/schema.graphql_.
+
+To start _NodeJS_ server with hot-reload on _PORT_ specified in `.env` file:
+
+```
+yarn server:dev
+```
+
+To build and start _NodeJS_ server.
+
+```
+yarn server:local
+```
+
+### Environment variables
+
+Specify envrionment variables in your `.env` file. Available variables are:
+
+- DOCUMENT_IDENTITY_SERVICE - url to [DOT Document server](https://developers.innovatrics.com/digital-onboarding/technical/remote/document-server/latest/documentation/.)
+- SERVER_PORT - [apollo-server-express](https://www.npmjs.com/package/apollo-server-express) port. Default value is 8000
+- AUTH_TOKEN - Authorization token for DIS API calls. In order to obtain AUTH_TOKEN, please [contact](https://developers.innovatrics.com/digital-onboarding/#contact-us) our sales department
+
